a2535d6c95c49eee7f02f5896585f1a511326449
Mathieu PATUREL <australie.p@gmail.com>
2022-07-12 21:05:35 +0200

    add resources and resource manager

---
+++ resource.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Resource(Enum):
+    DESERT = 0
+    CLAY = 1
+    WOOL = 2
+    HAY = 3
+    ROCK = 4
+    WOOD = 5
---
+++ resource_manager.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import pygame
+from resource import Resource
+
+class ResourceManager:
+
+    TILES: dict[Resource, pygame.Surface] = {}
+
+    @staticmethod
+    def load():
+        ResourceManager.TILES[Resource.WOOL] = pygame.image.load("images/TuileMouton.png")
+        ResourceManager.TILES[Resource.ROCK] = pygame.image.load("images/TuilePierre.png")
+        ResourceManager.TILES[Resource.DESERT] = pygame.image.load("images/TuileDesert.png")
+        ResourceManager.TILES[Resource.HAY] = pygame.image.load("images/TuileFoin.png")
+        ResourceManager.TILES[Resource.CLAY] = pygame.image.load("images/TuileArgile.png")
+        ResourceManager.TILES[Resource.WOOD] = pygame.image.load("images/TuileBois.png")
